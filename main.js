7c5a9028eca8f8d41f084de6919c9c03b2f7bd61
neilwave <georgreinecke@gmail.com>
2019-02-16 12:57:56 +0100

    1st push

---
+++ main.js
@@ -0,0 +1,8 @@
+// main javascript - evrything comes all together here
+
+buttonAction = () => {
+    document.getElementById('card-out').style.visibility='visible';
+}
+buttonActionRev = () => {
+    document.getElementById('card-out').style.visibility='hidden';
+}
