76a5546757643bc4cfde176a5ee1bbb6c1791969
neilwave <georgreinecke@gmail.com>
2019-02-25 23:17:42 +0100

    datamuse api

--- main.js
+++ main.js
@@ -1,18 +1,47 @@
 // main javascript
+
 // XMLHttp Request GET
-
-const xhr =  new XMLHttpRequest();
-const url = 'https://api.datamuse.com/endpoint';
-
-xhr.responseType = 'json';
-xhr.onreadystatechange = () => {
-    if(xhr.readyState === XMLHttpRequest.DONE) {
-        // return xhr.response;
-        console.log('I am fine, thank you!')
+//helper function for XMLHttpRequest
+const renderResponse = (res) => {
+    // Handles if res is falsey
+    if (!res) {
+        console.log(res.status);
     }
-};
-xhr.open('GET', url);
-xhr.send();
+    if (!res.length) {
+        console.log("<p>Try again!</p><p>There were no suggestions found!</p>");
+        return;
+    }
+    console.log(res.length);
+    // Creates an empty object and get filled with one random word
+    let wordList = {};
+    //creating words
+    let foo = Math.floor(Math.random() * res.length);
+    wordList = (res[foo].word);
+    document.getElementById('genfirst').innerHTML = `<h2>${wordList}</h2>`;
+} 
+
+// random list for api query ! a separate generator is needed here !
+const ranList = ['beach', 'mountain', 'soup', 'beer', 'tree', 'bike', 'hair'];
+let randomMyIndex = Math.floor(Math.random() * ranList.length);
+let ranIndy = ranList[randomMyIndex];
+// AJAX function
+const xhr = new XMLHttpRequest();
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jjb=';
+const randomList = ranIndy;
+
+const getNames = () => {
+    const endpoint = `${url}${queryParams}${ranIndy}`;
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            renderResponse(xhr.response)
+        }
+    };
+    xhr.open('GET', endpoint);
+    xhr.send();
+}
 
 // Name list
 const first = ['blues', 'sugar', 'pepp', 'small', 'big', 'handsome', 'thin'];
@@ -22,11 +51,11 @@ const last = ['sue', 'joe', 'bird', 'dan', 'dog', 'eagle', 'cat'];
 // function - generate random name
 genNames = (name) => {
 
-    if (name === first) {
-        let randomIndex = Math.floor(Math.random() * first.length);
-        let firstN = first[randomIndex];
-        return firstN;
-    }
+    // if (name === first) {
+    //     let randomIndex = Math.floor(Math.random() * first.length);
+    //     let firstN = first[randomIndex];
+    //     return firstN;
+    // }
     if (name === middle) {
         let randomIndex = Math.floor(Math.random() * middle.length);
         let midN = middle[randomIndex];
@@ -41,11 +70,12 @@ genNames = (name) => {
 } // end of genNames 
 myFunc = () => {
     // genNames function, generate random name, save to variable
-    const firstN = genNames(first);
+    // const firstN = genNames(first);
     const midN = genNames(middle);
     const lastN = genNames(last);
     // reveal generated names    
-    document.getElementById('genfirst').innerHTML = `<h2>${firstN}</h2>`;
+
+    // document.getElementById('genfirst').innerHTML = `<h2>${firstN}</h2>`;
     document.getElementById('genmid').innerHTML = `<h2>${midN}</h2>`;
     document.getElementById('genlast').innerHTML = `<h2>${lastN}</h2>`;
 }
@@ -160,15 +190,15 @@ colShaker = () => {
     document.getElementById('genmid').style.background = `rgb(${shaker.r_two},${shaker.g_two},${shaker.b_two})`;
     document.getElementById('genlast').style.background = `rgb(${shaker.r_three},${shaker.g_three},${shaker.b_three})`;
 }
+
 // function click button
 buttonAction = () => {
     // animation function, slot machine
     anim();
     // generate names with genNames through myFunc
     myFunc();
+    // new generator for names from datamuse API
+    getNames();
     // generate a rgb color for each color
     colShaker();
 } // end of button Action
-
-
-
